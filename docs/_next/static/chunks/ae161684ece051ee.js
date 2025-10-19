b9810f767b184e60756fd94f7ef73c28d258b68b
officialmailofdheeraj <officialmailofdheeraj@gmail.com>
2025-10-19 16:43:30 +0000

    update build scripts

--- docs/_next/static/chunks/ae161684ece051ee.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/b8f5a32d04fb0b00.js",
-  "static/chunks/37c32ed0a42f5a10.js",
-  "static/chunks/turbopack-03726b77b666b1ce.js"
-])
--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/sooraj-santhosh/" : "",
   basePath: isProd ? "/sooraj-santhosh" : "",
   output: "export",
   /* config options here */
