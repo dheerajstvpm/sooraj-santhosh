1d47154efc438c492ba39a382907d5d49d3d2ad4
officialmailofdheeraj <officialmailofdheeraj@gmail.com>
2025-10-19 09:24:24 +0000

    build files

---
+++ docs/_next/static/kQcFbISOkVFzU6byxdhBG/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/a9ffd34bc5314697.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
