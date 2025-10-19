01c493ae18d99d8eddd4e209ca92e1b9b9a60a7a
officialmailofdheeraj <officialmailofdheeraj@gmail.com>
2025-10-19 14:59:37 +0000

    build files

---
+++ docs/_next/static/chunks/96bb4b2d67e57f68.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/b8f5a32d04fb0b00.js",
+  "static/chunks/6a9302b1720ff2ca.js",
+  "static/chunks/turbopack-7872f7dd250ac770.js"
+])
---
+++ docs/_next/static/chunks/d11c82b1662fbbb1.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/f38feaa434ba2f91.js",
+  "static/chunks/6a9302b1720ff2ca.js",
+  "static/chunks/turbopack-e5097b44085bef39.js"
+])
