09470fa9a9fdc73a29aec5e4dd43f6a29798cc73
Dheeraj S <dheerajstvpm@gmail.com>
2025-10-29 21:57:20 +0530

    Replace 'next/image' with 'next/legacy/image' for compatibility in gallery and profile pages

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,5 @@
 import { Carousel, CarouselSlide } from "@mantine/carousel";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import image1 from "@/public/gallery/image1.jpg";
 import image2 from "@/public/gallery/image2.jpg";
 import image3 from "@/public/gallery/image3.jpg";
